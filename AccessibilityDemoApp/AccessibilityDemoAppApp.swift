9e9f3db5cbf39e0436f21809bd0c144e46b6189b
Jan Stusio <jan.stusio@protonmail.com>
2025-01-21 20:33:27 +0100

    Initial Commit

---
+++ AccessibilityDemoApp/AccessibilityDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityDemoAppApp.swift
+//  AccessibilityDemoApp
+//
+//  Created by Jan Stusio on 21/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
